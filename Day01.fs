fe9fc751d4886d566b4e789308c4cfae5a22c140
Mahmoud Mazouz <mazouz.mahmoud@outlook.com>
2022-12-01 13:25:22 +0100

    Day01: Fix typo and small nitpicks

--- Day01.fs
+++ Day01.fs
@@ -2,16 +2,18 @@ module AOC2022.Day01
 
 open System.IO
 
-let input = File.ReadAllText("data/input01.txt")
+let input = File.ReadAllText "data/input01.txt"
 let inventories = input.Split "\n\n"
 
 let totalCalories (inventory: string) =
-    inventory.TrimEnd().Split "\n" |> Seq.sumBy int
+    // NOTE: There is an unwanted trailing newline
+    let inventory = inventory.TrimEnd()
+    inventory.Split "\n" |> Seq.sumBy int
 
 let calories = inventories |> Array.map totalCalories
 
 // Part One
-let tolElf = calories |> Array.max
+let topElf = calories |> Array.max
 
 // Part Two
 let topThreeElves = calories |> Array.sortDescending |> Array.take 3 |> Array.sum
